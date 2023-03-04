12718ca90d3b0149b29a03b15fb07eba5a0665de
AleksandrBausher <bausheraleksandr@gmail.com>
2023-03-03 22:31:55 -0500

    ˇmade first page and started with second page already

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const tmdbAPIKEY = "e1a8dfba37492178ccf516d46b59d29d";
+const tmdbAPI = `https://api.themoviedb.org/3/movie/550?api_key=${tmdbAPIKEY}`;
+
+const giphyAPIKEY = "GX7LT5JHIOGBE4pfKzEHoaOYlVEw4aYN";
+const searchedMovie = "";
+const giphyAPI = `https://api.giphy.com/v1/gifs/search?api_key=${giphyAPIKEY}&q=${searchedMovie}&limit=9&offset=0&rating=g&lang=en`;
